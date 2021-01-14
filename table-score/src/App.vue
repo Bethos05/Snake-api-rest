ef8922d7db692280beae1ea5d0bd1f0b655fbfe8
Alberto Alvarez Herrera <alberto.alvarez512@gmail.com>
2021-01-13 22:57:46 -0500

    add project configuration and basic table using vuetify to visualize players and score

---
+++ table-score/src/App.vue
@@ -0,0 +1,22 @@
+<template> 
+  <v-app>
+    <v-main>
+      <table-score/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import TableScore from './components/TableScore.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    
+    TableScore
+  },
+
+};
+</script>
---
+++ table-score/src/components/TableScore.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Snake Score
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table class="data-table"
+      :headers="headers"
+      :items="players"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+</template>
+
+
+<script>
+//Import axios for http request
+import axios from "axios";
+
+export default {
+  //Data component vue
+  data() {
+    return {
+      search: "",
+      headers: [
+        { text: 'Nick Name', value: 'nickname'},
+        { text: 'Score', value: 'score' },
+      ],
+      players: [],
+    };
+  },
+  //get player when componet is mounted
+  mounted() {
+    this.getPlayers();
+  },
+  methods: {
+    //http request to obtain the players and their scores
+    getPlayers() {
+      axios
+        .get("http://localhost:8000/api/players/")
+        .then((response) => {
+            console.log(response)
+          this.players = response.data.players;
+        })
+        
+    },
+  },
+};
+</script>
+
+<style>
+    .v-card-title{
+        background: aqua;
+    }
+</style>
